efda936683448f08daf4ddd2d5cf6e9fe92c832d
Chandu Sasidharan <mail@chandu-sasidharan.de>
2024-03-21 11:23:48 +0100

    create conditional rendering

--- src/components/greetings/Greetings.jsx
+++ src/components/greetings/Greetings.jsx
@@ -1,4 +1,10 @@
 export default function Greetings({ name }) {
 
-    return <h1>Hello, { name }</h1>
+    return (
+       <div>
+         {/* { name ? <h1>Hello, { name }</h1> : null } */}
+
+         { !!name && <h1>Hello, {name} </h1>}
+       </div>
+    )
 }
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,7 +1,12 @@
+import Greetings from '../greetings/Greetings';
+
 export default function Hero() {
+    const firstName = 0;
+
     return (
-        <main className="pt-[60px] flex justify-center items-center min-h-screen">
-            <h1 className="text-xl">Hello world!</h1>
+        <main className="pt-[60px] flex flex-col gap-5 justify-center items-center min-h-screen">
+            <Greetings name={firstName} />
         </main>
     )
 }
+
