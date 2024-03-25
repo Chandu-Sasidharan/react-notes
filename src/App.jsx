1a257dc90af04fd8eda7dfc703591b07b1dd530a
Upasana <upasanadci@gmail.com>
2024-03-25 09:11:09 +0100

    new branch added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import reactLogo from './assets/react.svg';
+import reactLogo from "./assets/react.svg";
 
 export default function App() {
-    return (
-        <>
-            <h1 className="text-xl">Hello world!</h1>
-            <img src={reactLogo} alt="react logo" />
-            <img src="/vite.svg" alt="vite logo" />
-        </>
-    )
+  return (
+    <>
+      <h1 className="text-xl">Hello world!</h1>
+      <img src={reactLogo} alt="react logo" />
+      <img src="/vite.svg" alt="vite logo" />
+    </>
+  );
 }
