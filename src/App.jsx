fafb1c0d796294d28cdcd749739dd0d8610e918d
Upasana <upasanadci@gmail.com>
2024-03-25 09:32:58 +0100

    live coding added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import reactLogo from "./assets/react.svg";
+import Footer from "./component/Footer/footer";
+import Header from "./component/Header/header";
+import Main from "./component/Main/main";
 
 export default function App() {
   return (
     <>
-      <h1 className="text-xl">Hello world!</h1>
-      <img src={reactLogo} alt="react logo" />
-      <img src="/vite.svg" alt="vite logo" />
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
