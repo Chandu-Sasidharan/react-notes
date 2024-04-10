99d98547f3837c521076c1f83886fa51818e4776
Chandu Sasidharan <mail@chandu-sasidharan.de>
2024-04-10 10:11:26 +0200

    create browser router

--- README.md
+++ README.md
@@ -31,3 +31,24 @@ today:
   - All without reloading the page.
   - This is the foundation of client-side routing in SPAs!
 
+## React Router
+
+- React Router is a third-party library.
+- It's not included in React's core.
+- Powerful library to manage routing within your React projects.
+- You can install it with the command: `npm install react-router-dom`
+- Contains different kinds of routing, we will focus on `BrowserRouter`
+
+### Core components
+
+- **BrowserRouter**:
+  - A router that uses the HTML5 history API.
+  - Wraps your top-level React app.
+
+- **Routes and Route**: 
+  - Defines rules (which path should render which component).
+  - Routes is a container for multiple Route elements.
+  - Each Route specifies a component to render when the URL matches its path.
+
+- **Link**: 
+  - Replaces <a> tags for navigation within your SPA.
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import reactLogo from './assets/react.svg';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./pages/home";
+import About from "./pages/about";
 
 export default function App() {
     return (
-        <>
-            <h1 className="text-xl">Hello world!</h1>
-            <img src={reactLogo} alt="react logo" />
-            
-            {/* Stuffs in the public folder can be accessed from the root of the website */}
-            <img src="/vite.svg" alt="vite logo" />
-        </>
+        <BrowserRouter>
+            <nav className="navbar bg-slate-200">
+                <ul className="space-x-5">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                </ul>
+            </nav>
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
